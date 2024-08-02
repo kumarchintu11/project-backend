92c6a4dbe115dc2035bfdb4d1f65c55960e423e0
kumarchintu11 <159183397+kumarchintu11@users.noreply.github.com>
2024-08-02 13:03:40 +0530

    Database connection

---
+++ src/index.js
@@ -0,0 +1,63 @@
+// require('dotenv').config({path: './env'}) : becuase this require is ruining the program radability instead use import.
+
+import dotenv from "dotenv"; //  after import we need to config
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+// console.log('MONGODB_URI:', process.env.MONGODB_URI);  // Debugging :  it will let u not whether we are getting the url or not.
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants";
+
+// import express from "express";
+
+// const app = express();
+
+// m=1 : using function
+
+// function connectionBD(){}
+
+// connectionBD()
+
+
+// m-2 : using IIFE -- immediate execuatble function
+//  it`s a better approach.
+// ;(async() => {})() -- - here ; is used to clear before execution
+// (async() => {
+//     try{
+//         await mongoose.connect(`${process.env.MONGOBB-URI}/${DB_NAME}`);
+
+//         // to talk with the app
+//         app.on("error", (error) => {
+//             console.log("ERRRR", error);
+//             throw error;
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+//     }catch (error){
+//         console.log("ERROR: ", error);
+//         throw err
+//     }
+// })()
